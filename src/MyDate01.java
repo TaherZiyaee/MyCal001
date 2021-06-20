ac00bf4ee2d5e07183e9f9255e0a161cb87322de
Taher Ziyaei <ziaee.taher@gmail.com>
2021-06-21 02:36:05 +0330

    copy kiani program to MyCal001.MyDate01.

--- src/MyDate01.java
+++ src/MyDate01.java
@@ -1,105 +1,68 @@
 import com.ghasemkiani.util.PersianCalendarConstants;
 import com.ghasemkiani.util.icu.PersianCalendar;
+import com.ibm.icu.text.DateFormat;
+import com.ibm.icu.util.Calendar;
 
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
+import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.Locale;
 
 public class MyDate01 {
 
-    private PersianCalendar pc1,pc2;
-    private GregorianCalendar gc1,gc2;
+    private com.ibm.icu.util.Calendar pc1 = new PersianCalendar(new Date());
+    private com.ibm.icu.util.Calendar pc2 = new PersianCalendar(new Date());
 
-    private void definePersianRange() {
-        pc1 = new PersianCalendar(1400,PersianCalendarConstants.ORDIBEHESHT,01);
-        pc2 = new PersianCalendar(1400,PersianCalendarConstants.ORDIBEHESHT,22);
+    private void setPersianDate() {
+        pc1.set(1400, PersianCalendarConstants.KHORDAD,21);
+        pc2.set(1400,PersianCalendarConstants.TIR,20);
     }
 
-    public void printPersianDates() {
-        System.out.printf("Persian Date: %s\n",new MyDate01().setPersianFormat(pc1));
-    }
+    public int findFriday() {   // must be private
+        setPersianDate();
 
-    private String setPersianFormat(PersianCalendar calendar) {
-        calendar = new PersianCalendar();
-        com.ibm.icu.text.SimpleDateFormat fmt = new com.ibm.icu.text.SimpleDateFormat("yyyy/MM/dd");
-        fmt.setCalendar(calendar);
-        return fmt.format(calendar.getTime());
-    }
+        Locale loc = new Locale("FA","IR");
+        DateFormat df = pc1.getDateTimeFormat(DateFormat.DEFAULT,DateFormat.SHORT,loc);
+        DateFormat df1 = DateFormat.getDateInstance(pc1,DateFormat.DEFAULT,loc);
 
-    public void convertSHtoMIL() {
-        definePersianRange();
-        gc1 = new GregorianCalendar();
-        gc2 = new GregorianCalendar();
-        gc1.setTime(pc1.getTime());
-        gc2.setTime(pc2.getTime());
-
-        System.out.printf("Julian Date: %s\n",new MyDate01().setFormat(gc1));
-        System.out.printf("Julian Date: %s\n",new MyDate01().setFormat(gc2));
-    }
-
-    private String setFormat(GregorianCalendar calendar) {
-        SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd");
-        fmt.setCalendar(calendar);
-        return fmt.format(calendar.getTime());
-    }
-
-    public int findFriday() {
-        Calendar start = GregorianCalendar.getInstance();
-        start.setTime(pc1.getTime());
-        Calendar end = GregorianCalendar.getInstance();
-        end.setTime(pc2.getTime());
-
-        int numberOfFriday = 0;
-        int friday = Calendar.FRIDAY;
-
-        while (!start.after(end)) {
-            if (start.get(Calendar.DAY_OF_WEEK) == friday) {
-                numberOfFriday++;
-                System.out.printf("Friday #%d: %s\n",numberOfFriday,start.get(Calendar.DATE));
-                start.add(Calendar.DATE,7);
+        int numberOfFri = 0;
+        while (!pc1.after(pc2)) {
+            if (pc1.get(PersianCalendar.DAY_OF_WEEK) == PersianCalendar.FRIDAY) {
+                numberOfFri++;
+//                System.out.printf("Friday #%d: %s\n",numberOfFri,df.format(pc1.getTime()));
+                System.out.printf("Friday #%d: %s\n",numberOfFri,df1.format(pc1.getTime()));
+                pc1.add(PersianCalendar.DATE,7);
             } else
-                start.add(Calendar.DATE,1);
+                pc1.add(PersianCalendar.DATE,1);
         }
-        return numberOfFriday;
+        return numberOfFri;
     }
 
-    public int findThursday() {
-        Calendar start = GregorianCalendar.getInstance();
-        start.setTime(pc1.getTime());
-        Calendar end = GregorianCalendar.getInstance();
-        end.setTime(pc2.getTime());
+    public int findThursday() { // must be private
+        setPersianDate();
 
-        int numberOfThursday = 0;
-        int thursday = Calendar.THURSDAY;
-
-        while (!start.after(end)) {
-            if (start.get(Calendar.DAY_OF_WEEK) == thursday) {
-                numberOfThursday++;
-                System.out.printf("Thursday #%d: %s\n",numberOfThursday,start.get(Calendar.DATE));
-                start.add(Calendar.DATE,7);
+        int numberOfThu = 0;
+        while (!pc1.after(pc2)) {
+            if (pc1.get(com.ibm.icu.util.Calendar.DAY_OF_WEEK) == PersianCalendar.THURSDAY) {
+                numberOfThu++;
+                pc1.add(PersianCalendar.DATE,7); // 7 days later
             } else
-                start.add(Calendar.DATE,1);
+                pc1.add(PersianCalendar.DATE,1);
         }
-        return numberOfThursday;
+        return numberOfThu;
     }
 
-    public int findWednesday() {
-        Calendar start = GregorianCalendar.getInstance();
-        start.setTime(pc1.getTime());
-        Calendar end = GregorianCalendar.getInstance();
-        end.setTime(pc2.getTime());
+    public int findWednesday() { // must be private
+        setPersianDate();
 
-        int numberOfWednesday = 0;
-        int wednesday = Calendar.WEDNESDAY;
-
-        while (!start.after(end)) {
-            if (start.get(Calendar.DAY_OF_WEEK) == wednesday) {
-                numberOfWednesday++;
-                System.out.printf("Wednesday #%d: %s\n",numberOfWednesday,start.get(Calendar.DATE));
-                start.add(Calendar.DATE,7);
+        int numberOfWed = 0;
+        while (!pc1.after(pc2)) {
+            if (pc1.get(Calendar.DAY_OF_WEEK) == PersianCalendar.THURSDAY) {
+                numberOfWed++;
+                pc1.add(PersianCalendar.DATE,7); // 7 days later
             } else
-                start.add(Calendar.DATE,1);
+                pc1.add(PersianCalendar.DATE,1);
         }
-        return numberOfWednesday;
+        return numberOfWed;
     }
 }
