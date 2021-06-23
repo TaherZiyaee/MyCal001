74b9bdb50bc9926e3809a16d6217fd6570ee7832
taher <ziaee.taher@gmail.com>
2021-06-23 18:59:53 +0430

    first commit 14000202

--- src/MyDate01.java
+++ src/MyDate01.java
@@ -1,16 +1,15 @@
-import com.ghasemkiani.util.DateFields;
 import com.ghasemkiani.util.PersianCalendarConstants;
-import com.ghasemkiani.util.SimplePersianCalendar;
 import com.ghasemkiani.util.icu.PersianCalendar;
+import com.ghasemkiani.util.icu.PersianDateFormat;
 import com.ibm.icu.text.DateFormat;
 import com.ibm.icu.util.Calendar;
 
-import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
-import java.time.chrono.ChronoLocalDate;
-import java.time.temporal.ChronoUnit;
+
+import java.time.LocalDate;
+import java.time.chrono.HijrahChronology;
+import java.time.chrono.HijrahDate;
+import java.time.chrono.IsoChronology;
 import java.util.Date;
-import java.util.GregorianCalendar;
 import java.util.Locale;
 
 public class MyDate01 {
@@ -20,23 +19,21 @@ public class MyDate01 {
     private com.ibm.icu.util.Calendar pc2 = new PersianCalendar(new Date());
 
     private void setPersianDate() {
-        pc1.set(1400, PersianCalendarConstants.AZAR,21);
-        pc2.set(1400,PersianCalendarConstants.DEY,20);
+        pc1.set(1400, PersianCalendarConstants.KHORDAD,01);
+        pc2.set(1400,PersianCalendarConstants.KHORDAD,31);
     }
 
-    private int findFriday() {   // must be private
+    public int findFriday() {   // must be private
         setPersianDate();
 
         Locale loc = new Locale("FA","IR");
-        DateFormat df = pc1.getDateTimeFormat(DateFormat.DEFAULT,DateFormat.SHORT,loc);
-        DateFormat df1 = DateFormat.getDateInstance(pc1,DateFormat.DEFAULT,loc);
+        DateFormat df = PersianDateFormat.getDateInstance(pc1,PersianDateFormat.DEFAULT,loc);
 
         int numberOfFri = 0;
         while (!pc1.after(pc2)) {
             if (pc1.get(PersianCalendar.DAY_OF_WEEK) == PersianCalendar.FRIDAY) {
                 numberOfFri++;
 //                System.out.printf("Friday #%d: %s\n",numberOfFri,df.format(pc1.getTime()));
-                System.out.printf("Friday #%d: %s\n",numberOfFri,df1.format(pc1.getTime()));
                 pc1.add(PersianCalendar.DATE,7);
             } else
                 pc1.add(PersianCalendar.DATE,1);
@@ -44,7 +41,7 @@ public class MyDate01 {
         return numberOfFri;
     }
 
-    private int findThursday() { // must be private
+    public int findThursday() { // must be private
         setPersianDate();
 
         int numberOfThu = 0;
@@ -58,7 +55,7 @@ public class MyDate01 {
         return numberOfThu;
     }
 
-    private int findWednesday() { // must be private
+    public int findWednesday() { // must be private
         setPersianDate();
 
         int numberOfWed = 0;
@@ -81,5 +78,45 @@ public class MyDate01 {
             pc1.add(PersianCalendar.DATE, 1); // 1 days later
         }
         return numberOfDays;
+    }
+
+    public LocalDate hijriToGregorian() {
+        HijrahDate date = HijrahChronology.INSTANCE.dateNow();
+        System.out.println(date);
+
+        LocalDate gDate = IsoChronology.INSTANCE.date(date);
+        System.out.println(gDate);
+
+        int year = gDate.getYear();
+//        String month = String.valueOf(gDate.getMonth());
+        int month = gDate.getMonthValue();
+        int day = gDate.getDayOfMonth();
+        System.out.printf("Y: %d, M: %d, D: %d\n",year,month,day);
+
+        return gDate;
+    }
+
+    public void gregorianToShamsi() {
+
+    }
+
+    public boolean findDayBetween() {
+        setPersianDate();
+        Calendar cal = new PersianCalendar();
+        cal.set(1400,PersianCalendarConstants.MEHR,15);
+        boolean condition = false;
+        /*if (cal.after(pc1) && cal.before(pc2))
+            return true;
+        else
+            return false;*/
+        while (!pc1.after(pc2)) {
+            if (pc1.get(Calendar.DATE) == cal.get(Calendar.DATE))
+//            if (pc1.getTime().equals(cal.getTime()))
+//                condition = true;
+                return true;
+            else
+                pc1.add(PersianCalendar.DATE,1);
+        }
+        return false;
     }
 }
--- src/MyMain.java
+++ src/MyMain.java
@@ -3,7 +3,11 @@ public class MyMain {
     public static void main(String[] args) {
         MyDate01 myDate01 = new MyDate01();
 
-        System.out.println(myDate01.countDays());
+//        System.out.println("Fridays#: " + myDate01.findFriday());
+//        System.out.println("Thursday#: " + myDate01.findThursday());
+//        System.out.println("Wednesday#: " + myDate01.findWednesday());
+//        System.out.println("All Days: " + myDate01.countDays());
 
+        System.out.println(myDate01.findDayBetween());
     }
 }
